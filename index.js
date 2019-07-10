b1930b55624f3e44787d068071be5a904e88b0d9
dxcweb <dxcweb@qq.com>
2019-07-10 15:36:17 +0800

    no message

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const master = require('./master');
-const url = 'http://120.25.154.88:9000/admin/user/me';
-const timelimit = 10;
+const master = require("./master");
+const url = "https://api-search.720yun.com/search?channelId=0&content=%E5%A4%A7%E5%AD%A6&page=40&selected=2&type=1";
+const timelimit = 60;
 const maxSockets = 100;
-master(url, timelimit, maxSockets, (result) => {
-	console.log(result);
+const headers = {
+  referer: "https://720yun.com/channel",
+  origin: "https://720yun.com",
+  "app-key": "eByjUyLDG2KtkdhuTsw2pY46Q3ceBPdT",
+};
+master(url, headers, timelimit, maxSockets, (result) => {
+  console.log(result);
 });
--- worker.js
+++ worker.js
@@ -1,47 +1,51 @@
-const http = require('http');
-const https = require('https');
-const { parse } = require('url');
+const http = require("http");
+const https = require("https");
+const { parse } = require("url");
 
-function worker(url, timelimit, maxSockets, callback) {
-	const options = parse(url);
-	let request;
-	if (options.protocol == 'http:') {
-		request = http.request;
-	} else {
-		request = https.request;
-	}
-	const keepAliveAgent = new http.Agent({ keepAlive: true, maxSockets });
-	options.agent = keepAliveAgent;
-	options.headers = {
-		'User-Agent':
-			'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
-	};
-	let map = {};
-	let bool = true;
-	function execute() {
-		const req = request(options, (res) => {
-			if (!map[res.statusCode]) {
-				map[res.statusCode] = 1;
-			} else {
-				map[res.statusCode]++;
-			}
-			res.on('data', () => {});
-			res.on('end', () => {});
-			if (bool) {
-				execute();
-			}
-		});
-		req.end();
-		req.on('error', () => {});
-	}
-	for (let i = 0; i < maxSockets * 1.5; i++) {
-		execute();
-	}
-	setTimeout(() => {
-		keepAliveAgent.requests = {};
-		keepAliveAgent.destroy();
-		callback(map);
-	}, timelimit * 1000);
+function worker(url, headers, timelimit, maxSockets, callback) {
+  const options = parse(url);
+  let request;
+  let Agent;
+  if (options.protocol == "http:") {
+    request = http.request;
+    Agent = http.Agent;
+  } else {
+    request = https.request;
+    Agent = https.Agent;
+  }
+  const keepAliveAgent = new Agent({ keepAlive: true, maxSockets });
+  options.agent = keepAliveAgent;
+  options.headers = {
+    "User-Agent":
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
+    ...headers,
+  };
+  let map = {};
+  let bool = true;
+  function execute() {
+    const req = request(options, (res) => {
+      if (!map[res.statusCode]) {
+        map[res.statusCode] = 1;
+      } else {
+        map[res.statusCode]++;
+      }
+      res.on("data", () => {});
+      res.on("end", () => {});
+      if (bool) {
+        execute();
+      }
+    });
+    req.end();
+    req.on("error", () => {});
+  }
+  for (let i = 0; i < maxSockets * 1.5; i++) {
+    execute();
+  }
+  setTimeout(() => {
+    keepAliveAgent.requests = {};
+    keepAliveAgent.destroy();
+    callback(map);
+  }, timelimit * 1000);
 }
 
 module.exports = worker;
